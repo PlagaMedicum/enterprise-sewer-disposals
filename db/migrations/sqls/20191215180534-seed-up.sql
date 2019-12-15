99ec51975b3f5c8f5b60a9edfd6db4da5dc429fd
PlagaMedicum <katoshrodingera@protonmail.ch>
2019-12-15 22:26:45 +0300

    improved migrations

--- db/migrations/sqls/20191215180534-seed-up.sql
+++ db/migrations/sqls/20191215180534-seed-up.sql
@@ -3,20 +3,29 @@ CREATE TABLE IF NOT EXISTS enterprises(
     name TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS enterprises_discharges(
-
+    e_id BIGINT NOT NULL,
+    d_id BIGINT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS discharges(
-
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS background_concentrations(
-
+    id BIGINT NOT NULL,
+    date DATE NOT NULL,
+    concentration INT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS concentration_limits(
-
+    id BIGINT NOT NULL,
+    date DATE NOT NULL,
+    concentration INT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS discharges_drains(
-
+    dis_id BIGINT NOT NULL,
+    drain_id BIGINT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS drain_concentrations(
-    
+    id BIGINT NOT NULL,
+    date DATE NOT NULL,
+    concentration INT NOT NULL
 );
