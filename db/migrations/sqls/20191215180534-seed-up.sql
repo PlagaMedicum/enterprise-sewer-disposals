e799e0cff7bb6b11b704821c85c9e2eeda6cbc45
PlagaMedicum <katoshrodingera@protonmail.ch>
2019-12-15 21:36:29 +0300

    updated .gitignore

--- db/migrations/sqls/20191215180534-seed-up.sql
+++ db/migrations/sqls/20191215180534-seed-up.sql
@@ -2,9 +2,21 @@ CREATE TABLE IF NOT EXISTS enterprises(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
-DROP TABLE IF EXISTS enterprises_discharges;
-DROP TABLE IF EXISTS discharges;
-DROP TABLE IF EXISTS background_concentrations;
-DROP TABLE IF EXISTS concentration_limits;
-DROP TABLE IF EXISTS discharges_drains;
-DROP TABLE IF EXISTS drain_concentrations;
+CREATE TABLE IF NOT EXISTS enterprises_discharges(
+
+);
+CREATE TABLE IF NOT EXISTS discharges(
+
+);
+CREATE TABLE IF NOT EXISTS background_concentrations(
+
+);
+CREATE TABLE IF NOT EXISTS concentration_limits(
+
+);
+CREATE TABLE IF NOT EXISTS discharges_drains(
+
+);
+CREATE TABLE IF NOT EXISTS drain_concentrations(
+    
+);
